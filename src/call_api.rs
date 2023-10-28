9593a943996058bacdb6071b5cc782a0b628ccd7
tochiji <7910558+tochiji@users.noreply.github.com>
2023-10-28 17:25:00 +0900

    モデルをgpt-4へ変更

--- src/call_api.rs
+++ src/call_api.rs
@@ -50,7 +50,7 @@ fn generate_body_from_history(message_history: &chat_message::MessageHistory) ->
     let body = json!({
         "top_p": 0.5,
         "stream": true, // streamとして受け取る設定
-        "model": "gpt-3.5-turbo-16k",
+        "model": "gpt-4",
         "messages": messages,
     });
     body
