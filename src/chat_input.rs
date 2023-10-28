552314166238219e84f5cd369e4f22f9d6648acb
tochiji <7910558+tochiji@users.noreply.github.com>
2023-10-28 17:23:54 +0900

    文字列の入力方法を変更

--- src/chat_input.rs
+++ src/chat_input.rs
@@ -1,11 +1,28 @@
-use std::io::Read;
+use rustyline::{error::ReadlineError, DefaultEditor};
 
-// 標準入力から文字列を読み込む関数
-// この関数が実行されると、Ctrl+Dが入力されるまでメインスレッドがストップする。
+// 標準入力から複数行の文字列を読み込む関数
 pub fn stdin_to_string() -> Result<String, Box<dyn std::error::Error>> {
-    let stdin = std::io::stdin(); // 標準入力を取得
-    let mut reader = stdin.lock(); // 標準入力をロックして、読み込み用のreaderを作成
+    let mut rl = DefaultEditor::new()?; // rustylineのインスタンスを作成
+
     let mut buffer = String::new(); // 読み込んだ文字列を格納するためのバッファを作成
-    reader.read_to_string(&mut buffer)?; // readerから文字列を読み込み、バッファに格納する。エラーが発生した場合は即座に関数から抜ける。
+    loop {
+        let readline = rl.readline(""); // プロンプトを表示せずにユーザーからの入力を待つ
+        match readline {
+            Ok(line) => {
+                buffer.push_str(&line); // 入力された行をバッファに追加
+                buffer.push('\n'); // 改行をバッファに追加
+            }
+            Err(ReadlineError::Interrupted) => {
+                std::process::exit(0); // Ctrl+Cで終了
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
     Ok(buffer) // 読み込みに成功した場合は、バッファをOkで返す。
 }
