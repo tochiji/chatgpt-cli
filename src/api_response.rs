04def62aadfc3060d8668ad3923718b42d826eb0
tochiji <7910558+tochiji@users.noreply.github.com>
2023-11-12 23:31:27 +0900

    modelレスポンス型を追加

--- src/api_response.rs
+++ src/api_response.rs
@@ -1,5 +1,53 @@
 use serde::Deserialize;
 
+///
+/// ChatGPTのmodel一覧を取得するAPIのレスポンス
+///
+/// ```json
+/// // sample response
+/// {
+///   "object": "list",
+///   "data": [
+///     {
+///       "id": "text-search-babbage-doc-001",
+///       "object": "model",
+///       "created": 1651172509,
+///       "owned_by": "openai-dev"
+///     },
+///     {
+///       "id": "gpt-3.5-turbo-16k-0613",
+///       "object": "model",
+///       "created": 1685474247,
+///       "owned_by": "openai"
+///     },
+///    ...
+///  ]
+/// }
+/// ```
+///
+#[derive(Debug, Deserialize)]
+pub struct Models {
+    pub object: String,
+    pub data: Vec<Model>,
+}
+
+impl Models {
+    pub fn get_gpts(&self) -> Vec<String> {
+        let mut filterd: Vec<&Model> = self.data.iter().filter(|m| m.id.contains("gpt-")).collect();
+        filterd.sort_by_key(|m| m.created);
+        filterd.reverse();
+        filterd.iter().map(|m| m.id.clone()).collect()
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Model {
+    pub id: String,
+    pub object: String,
+    pub created: u64,
+    pub owned_by: String,
+}
+
 // 下記のようなデータが返ってくる
 //
 // ```
