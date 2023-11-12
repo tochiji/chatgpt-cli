c20dc143eee7d55c893afad1b351fe7fb6b7e140
tochiji <7910558+tochiji@users.noreply.github.com>
2023-11-12 23:36:05 +0900

    Roleを追加

--- src/chat_message.rs
+++ src/chat_message.rs
@@ -1,28 +1,49 @@
+use std::fmt;
+
+#[derive(Debug, Default)]
+/// APIに送るメッセージ履歴
+///
+/// ChatGPTへの初期プロンプトおよびユーザーからの質問、ChatGPTからの回答が、順次格納される。
+/// この履歴を元に、ChatGPTは次の回答を生成する。
 pub struct MessageHistory {
     pub messages: Vec<Message>,
 }
 
 impl MessageHistory {
-    pub fn new() -> Self {
-        Self { messages: vec![] }
+    pub fn push(&mut self, role: Role, content: &str) {
+        let m = Message::new(role, content);
+        self.messages.push(m);
     }
+}
 
-    pub fn push(&mut self, role: &str, content: &str) {
-        self.messages.push(Message::new(role, content));
+#[derive(Debug, Clone)]
+pub enum Role {
+    User,
+    System,
+    Assistant,
+}
+
+impl fmt::Display for Role {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Role::User => write!(f, "user"),
+            Role::System => write!(f, "system"),
+            Role::Assistant => write!(f, "assistant"),
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Message {
-    pub role: String,
+    pub role: Role,
     pub content: String,
 }
 
 impl Message {
-    pub fn new(role: &str, content: &str) -> Self {
+    pub fn new(role: Role, content: &str) -> Self {
         Self {
-            role: role.to_string(),
-            content: content.to_string(),
+            role,
+            content: content.to_owned(),
         }
     }
 }
